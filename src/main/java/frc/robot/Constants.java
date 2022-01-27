a211c2bbe394b83f73ebcc132efb120e25ed1589
Andrew Barton <abars@barleyfarm.com>
2022-01-26 20:17:28 -0600

    Add Ramsete and default trajectory

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -41,6 +43,22 @@ public final class Constants {
 
     public static final int kControllerA = 0;
 
+
+    //Characterization Constraints
+    public static final double ksVolts = 0.5805;
+    public static final double kvVoltSecondsPerMeter = 5.9;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.63211;
+    public static final double kPDriveVel = 7.524;
+    public static final double kTrackwidthMeters = 0.6096; //24 inches
+    public static final double kMaxSpeedMetersPerSecond = 1.09; //3 feet per second
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static final double kRamseteB = 2;
+    public static final double kRamseteZeta = 0.7;
+
+    public static final DifferentialDriveKinematics kDriveKinematics =
+    new DifferentialDriveKinematics(kTrackwidthMeters);
+
+    
     // Drive
 
     public static final double ksVolts = 0.5805;
