f52d3ae82df72f21fe51f31eafd88a7faebf5e35
Andrew Barton <abars@barleyfarm.com>
2022-02-16 20:26:32 -0600

    Cut code down to just driving

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,8 +12,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.DriveWithGamepadCommand;
-import frc.robot.commands.RunConveyorCommand;
-import frc.robot.commands.RunShooterCommand;
 import frc.robot.subsystems.*;
 
 /**
@@ -25,12 +23,6 @@ import frc.robot.subsystems.*;
 public class RobotContainer {
   // Subsystems
   private final Drive m_drive = new Drive();
-  private final Conveyor m_conveyor = new Conveyor();
-  private final Shooter m_shooter = new Shooter();
-
-  private final FrontClimber m_frontClimber = new FrontClimber();
-
-  private final BackClimber m_backClimber = new BackClimber();
 
   private final XboxController m_controller = new XboxController(Constants.kControllerA);
 
@@ -40,11 +32,7 @@ public class RobotContainer {
     configureButtonBindings();
 
     m_drive.setDefaultCommand(new DriveWithGamepadCommand(m_drive,m_controller));
-    JoystickButton conveyorUp = new JoystickButton(m_controller, XboxController.Button.kLeftBumper.value);
-    conveyorUp.whileHeld(new RunConveyorCommand(m_conveyor, RunConveyorCommand.Direction.kUp));
-
-    JoystickButton conveyorDown = new JoystickButton(m_controller, XboxController.Button.kRightBumper.value);
-    conveyorDown.whileHeld(new RunConveyorCommand(m_conveyor, RunConveyorCommand.Direction.kDown));
+    
 
   }
 
@@ -56,26 +44,7 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
 
-    JoystickButton rpmButton1 = new JoystickButton(m_controller, XboxController.Button.kA.value);
-    rpmButton1.whileHeld(new RunShooterCommand(m_shooter, 1500));
-
-    JoystickButton rpmButton2 = new JoystickButton(m_controller, XboxController.Button.kX.value);
-    rpmButton2.whileHeld(new RunShooterCommand(m_shooter, 2000));
-
-    JoystickButton rpmButton3 = new JoystickButton(m_controller, XboxController.Button.kB.value);
-    rpmButton3.whileHeld(new RunShooterCommand(m_shooter, 2500));
-
-    /*JoystickButton ForwardClimbClockwiseButton = new JoystickButton(m_controller, XboxController.Button.kY.value);
-    ForwardClimbClockwiseButton.whileHeld(new ForwardClimbClockwise(m_frontClimber));
-
-    JoystickButton ForwardClimbCounterClockwiseButton = new JoystickButton(m_controller, XboxController.Button.kB.value);
-    ForwardClimbCounterClockwiseButton.whileHeld(new ForwardClimbCounterClockwise(m_frontClimber));
-
-    JoystickButton BackwardClimbClockwiseButton = new JoystickButton(m_controller, XboxController.Button.kX.value);
-    BackwardClimbClockwiseButton.whileHeld(new BackwardClimbClockwise(m_backClimber));
-
-    JoystickButton BackwardClimbCounterClockwiseButton = new JoystickButton(m_controller, XboxController.Button.kA.value);
-    BackwardClimbCounterClockwiseButton.whileHeld(new BackwardClimbCounterClockwise(m_backClimber));*/
+   
   }
 
   /**
@@ -85,22 +54,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // Return null for no autonomous command
-    return new ParallelCommandGroup(
-            new RunShooterCommand(m_shooter, 2500),
-            new SequentialCommandGroup(
-                    new WaitCommand(2),
-                    new RunConveyorCommand(m_conveyor, RunConveyorCommand.Direction.kUp).withTimeout(5)
-            )
-    );
-
-//    Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-//            new Pose2d(0, 0, new Rotation2d(0)),
-//            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-//            new Pose2d(3, 0, new Rotation2d(0)),
-//            Constants.kTrajectoryConfig
-//    );
-//
-//    return new DriveAlongTrajectoryCommand(m_drive, trajectory)
-//            .andThen(() -> m_drive.tankDriveVolts(0, 0));
+    return null;
   }
 }
