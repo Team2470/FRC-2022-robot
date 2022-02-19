072893e25ad382088369ceb87a9cf8d2db370a0e
Joseph Shetaye <josephs@moreperfect.co>
2022-02-19 10:54:05 -0600

    Finish intake subsystem prototype

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,5 +15,24 @@ public class Intake extends SubsystemBase {
     public Intake() {
         m_intakeMotor = new CANSparkMax(Constants.kIntakeMotorId, MotorType.kBrushless);
         m_deploy = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.kIntakeSolenoid);
+
+        m_intakeMotor.setSmartCurrentLimit(40);
+        m_intakeMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+    }
+
+    public void lowerIntake() {
+        m_deploy.set(true);
+    }
+
+    public void raiseIntake() {
+        m_deploy.set(false);
+    }
+
+    public void startIntakeMotor(int direction, double speed) {
+        m_intakeMotor.set(direction * speed);
+    }
+
+    public void stopIntakeMotor() {
+        m_intakeMotor.stopMotor();
     }
 }
