ebf37b65fe39042c4a8457bf3909ee6b74ef0729
Andrew Barton <abars@barleyfarm.com>
2022-04-01 20:01:13 -0500

    Optimize stepping

--- src/main/java/frc/robot/commands/ClimbSequenceCommandGroup.java
+++ src/main/java/frc/robot/commands/ClimbSequenceCommandGroup.java
@@ -6,9 +6,11 @@ import edu.wpi.first.util.sendable.SendableBuilder.BackendKind;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.BackClimber;
 import frc.robot.subsystems.FrontClimber;
 
@@ -17,64 +19,40 @@ import java.util.Map;
 import java.util.zip.Deflater;
 
 public class ClimbSequenceCommandGroup extends SequentialCommandGroup {
-  private static ClimbState m_climbState = ClimbState.kStartingConfig;
-  private static BarState m_barState = BarState.kNone;
+  private static ClimbState m_climbState = ClimbState.kSequence1;
+  private static BarState m_barState = BarState.kMid;
 
   public ClimbSequenceCommandGroup(FrontClimber frontClimber, BackClimber backClimber) {
     addCommands(
         new SelectCommand(
             Map.ofEntries(
-                // Starting Config
-                // Front out (65), back out (90)
-                Map.entry(ClimbState.kStartingConfig, new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(90))),
-                // Pull up
-                // Front in (limit)
-                Map.entry(ClimbState.kPullUpFront, new MoveFrontClimberCommand(frontClimber, Rotation2d.fromDegrees(15))),
-                // Handover
-                // Back in (30 -> limit)
-                Map.entry(ClimbState.kHandover, new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(30))),
-                // Swing to clear bar
-                // Front out (65), back out (90)
-                Map.entry(ClimbState.kSwingToClear, new MoveFrontClimberCommand(frontClimber, Rotation2d.fromDegrees(40))),
-                // Out to catch bar
-                // Front out (110)
-                Map.entry(ClimbState.kReadyToCatch, new SequentialCommandGroup(
+                // Fully auto section
+                Map.entry(ClimbState.kSequence1, new SequentialCommandGroup(
                   new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(90)),
-                  new MoveFrontClimberCommand(frontClimber, Rotation2d.fromDegrees(110)))
-                ),
-                // Pull up
-                // Back in (30 -> limit)
-                Map.entry(ClimbState.kPullUpBack, new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(30))),
+                  new MoveFrontClimberCommand(frontClimber, Rotation2d.fromDegrees(10)),
+                  new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(30)), // Handover
+                  new ParallelCommandGroup(
+                    new MoveFrontClimberCommand(frontClimber, Rotation2d.fromDegrees(110)),
+                    new SequentialCommandGroup(
+                      new WaitUntilCommand(() -> frontClimber.getAngle().getDegrees() >= 20),
+                      new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(75))
+                    )
+                  ),
+                  new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(30))
+                )),
                 // Grab bar
-                // Front in (100)
                 Map.entry(ClimbState.kGrabBar, new MoveFrontClimberCommand(frontClimber, Rotation2d.fromDegrees(95))),
-                // Drop bar
-                // Back out (125)
-                Map.entry(ClimbState.kDropBar, new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(125))),
-                //Move back out of the way
-                //Back in (limit)
-                Map.entry(ClimbState.kMoveBack, new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(30))),
                 // Pull up
-                // Front in (limit), back in (90)
-                Map.entry(ClimbState.kPullUp, new ParallelCommandGroup(
-                    new MoveFrontClimberCommand(frontClimber, Rotation2d.fromDegrees(10)),
-                    new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(90)))
-                    ),
-                // Handover
-                // Back in (30 -> limit)
-
-                // Swing to clear traverse bar
-                // Front out (65), back out (90)
-
-                // Out to catch traverse bar
-                // Front out (110)
-
-                // Grab traverse bar
-                // Front in (100)
-
-                // Drop highbar
-                // Back out (125)
-
+                Map.entry(ClimbState.kPullUp, new SequentialCommandGroup(
+                  new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(125)),
+                  new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(30)),
+                  new ParallelCommandGroup(
+                      new MoveFrontClimberCommand(frontClimber, Rotation2d.fromDegrees(10)),
+                      new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(90)))
+                  )
+                ),
+                // Drop traverse
+                Map.entry(ClimbState.kDropTraverse, new BackClimbAngleCommand(backClimber, Rotation2d.fromDegrees(125))),
                 //Finish on traverse
                 Map.entry(ClimbState.kDone, new PrintCommand("Climb Done"))
             ),
@@ -84,56 +62,38 @@ public class ClimbSequenceCommandGroup extends SequentialCommandGroup {
   }
 
   public static void reset() {
-    m_climbState = ClimbState.kStartingConfig;
-    m_barState = BarState.kNone;
+    m_climbState = ClimbState.kSequence1;
+    m_barState = BarState.kMid;
     SmartDashboard.putString("Arm State", m_climbState.name());
     SmartDashboard.putString("Bar", m_barState.name());
   }
 
   public void advanceState() {
     switch (m_climbState) {
-      case kStartingConfig:
-        m_climbState = ClimbState.kPullUpFront;
-        m_barState = BarState.kMid;
-        break;
-      case kPullUpFront:
-        m_climbState = ClimbState.kHandover;
-        break;
-      case kHandover:
-        m_climbState = ClimbState.kSwingToClear;
-        break;
-      case kSwingToClear:
-        m_climbState = ClimbState.kReadyToCatch;
-        break;
-      case kReadyToCatch:
-        m_climbState = ClimbState.kPullUpBack;
-        break;
-      case kPullUpBack:
+      case kSequence1:
         m_climbState = ClimbState.kGrabBar;
         break;
-      case kGrabBar:
-        m_climbState = ClimbState. kDropBar;
-        break;
-      case kDropBar:    
+      case kGrabBar: 
         switch (m_barState) {
           case kMid:
             m_barState = BarState.kHigh;
-            m_climbState = ClimbState.kMoveBack;
+            m_climbState = ClimbState.kPullUp;
             break;
           case kHigh:
             m_barState = BarState.kTraverse;
-            m_climbState = ClimbState.kDone;
+            m_climbState = ClimbState.kDropTraverse;
             break;
-          default:
-            m_climbState = ClimbState.kStartingConfig;
+          case kTraverse:
             break;
         }
         break;
-      case kMoveBack:
-        m_climbState = ClimbState.kPullUp;
-        break;
       case kPullUp:
-        m_climbState = ClimbState.kHandover;
+        m_climbState = ClimbState.kSequence1;
+        break;
+      case kDropTraverse:
+        m_climbState = ClimbState.kDone;
+        break;
+      case kDone:
         break;
     }
     SmartDashboard.putString("Arm State", m_climbState.name());
@@ -143,21 +103,14 @@ public class ClimbSequenceCommandGroup extends SequentialCommandGroup {
   public ClimbState getClimbState() { return m_climbState; }
 
   private enum ClimbState {
-    kStartingConfig, // F65 B90
-    kPullUpFront, // F0, B90
-    kHandover, // B30->0
-    kSwingToClear, // F65, B90
-    kReadyToCatch, // F110
-    kPullUpBack, // B30->0
+    kSequence1, // B30->0
     kGrabBar, // F100
-    kDropBar, // B125
-    kMoveBack,  //B30
+    kDropTraverse,
     kPullUp,   //F10 B90
     kDone
   }
 
   private enum BarState {
-    kNone,
     kMid,
     kHigh,
     kTraverse
